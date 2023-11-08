8549341cd4e95cf71a088f514da00199a576df09
Syahrul02 <syhrl44@gmail.com>
2023-11-08 09:38:34 +0700

    push router

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,11 +1,23 @@
-import "./style.css"
+import { Button } from "react-bootstrap";
+import "./style.css";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/berita");
+  };
+
   return (
-    <div className='home'>
-        <h1>Belajar React Router</h1>
+    <div className="home">
+      <h1 className="mt-5">Belajar React Router</h1>
+
+      <Button className="mt-5" onClick={handleClick} variant="primary">
+        Menuju ke halaman berita
+      </Button>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
