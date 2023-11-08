560f0a3ea92e702c3ac94330ebbea2b4c5630956
Syahrul02 <syhrl44@gmail.com>
2023-11-08 09:30:51 +0700

    push router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import './App.css'
-import BeritaClass from './BeritaClassComponent'
+import { Link, Outlet } from "react-router-dom";
+import "./App.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 function App() {
-
   return (
-    <>
-      <div className='test'>
-        <BeritaClass/>
-      </div>
-    </>
-  )
+    <div>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand to="/home">Eduwork</Navbar.Brand>
+          <Nav className="me-auto">
+            <Link className="me-3" to="/home">Home</Link>
+            <Link className="me-3" to="/berita">Berita</Link>
+            <Link to="/form">Form</Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Outlet />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home/index.jsx';
+import Berita from './Berita/index.jsx';
+import Form from './Form/index.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="home" element={<Home />} />
+          <Route path="berita" element={<Berita />} />
+          <Route path="form" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
